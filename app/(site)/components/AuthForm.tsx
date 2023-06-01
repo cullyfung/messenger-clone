6a64725b33d3e4b5472877a105ed88bb339407fa
cullyfung <cullyfung@gamil.com>
2023-06-02 02:02:57 +0800

    feat: sidebar & navigation layout

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import axios from 'axios'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
 import Button from '@/app/components/Button'
@@ -9,13 +9,23 @@ import Input from '@/app/components/inputs/Input'
 import AuthSocialButton from './AuthSocialButton'
 import { BsGithub, BsGoogle } from 'react-icons/bs'
 import { toast } from 'react-hot-toast'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
 const AuthForm = () => {
+  const session = useSession()
+  const router = useRouter()
   const [variant, setVariant] = useState<Variant>('LOGIN')
   const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      console.log('Authenticated')
+      router.push('/users')
+    }
+  }, [session?.status, router])
 
   const toggleVariant = useCallback(() => {
     if (variant === 'LOGIN') {
@@ -43,6 +53,7 @@ const AuthForm = () => {
     if (variant === 'REGISTER') {
       axios
         .post('/api/register', data)
+        .then(() => signIn('credentials', data))
         .catch(() => toast.error('Something went wrong!'))
         .finally(() => setIsLoading(false))
     }
@@ -59,6 +70,7 @@ const AuthForm = () => {
 
           if (callback?.ok && !callback?.error) {
             toast.success('Logged in!')
+            router.push('/users')
           }
         })
         .finally(() => setIsLoading(false))
@@ -78,6 +90,7 @@ const AuthForm = () => {
 
         if (callback?.ok && !callback?.error) {
           toast.success('Logged in!')
+          router.push('/users')
         }
       })
       .finally(() => setIsLoading(false))
